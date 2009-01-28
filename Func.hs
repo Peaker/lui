2791f3da7179d9921967ce3b6542e3d3f7a25f99
eyal <eyal.lotem@gmail.com>
2009-01-28 20:30:21 +0200

    ghc 6.8 compatible

--- Func.hs
+++ Func.hs
@@ -3,15 +3,14 @@
 
 module Func where
 
-import Prelude hiding ((.))
-import Control.Category(Category, (.))
+import Control.Arrow(Arrow, (<<<), (>>>))
 
-result :: Category cat => cat b c -> cat a b -> cat a c
-result = (.)
+result :: Arrow arr => arr b c -> arr a b -> arr a c
+result = (<<<)
 
-argument :: Category cat => cat a b -> cat b c -> cat a c
-argument = flip (.)
+argument :: Arrow arr => arr a b -> arr b c -> arr a c
+argument = (>>>)
 
 infixr 2 ~>
-(~>) :: (Category cat) => cat a b1 -> cat b c -> cat b1 b -> cat a c
-(arg ~> res) func = res . func . arg
+(~>) :: (Arrow arr) => arr a b -> arr c d -> arr b c -> arr a d
+(arg ~> res) func = arg >>> func >>> res
